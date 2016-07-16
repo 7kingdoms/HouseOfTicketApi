67bfaf42134fd3fbb1e3c58c528df632581f4ebb
Wow <mayjii555@gmail.com>
2016-07-16 15:08:44 +0700

    no message

---
+++ config/2c2p.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'merchant_id' => '764764000000014',  
+  '2c2p_secret' => '1JA8CNahHfzs', 
+  '2c2p_paymenturl' => 'https://demo2.2c2p.com/2C2PFrontEnd/RedirectV3/payment', 
+
+	];
+	
