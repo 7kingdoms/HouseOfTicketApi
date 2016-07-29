ad47a85ef8d85e2c9355b83ca2458a1c7d50111d
Wow <mayjii555@gmail.com>
2016-07-29 15:56:08 +0700

    payment boonterm price

--- app/Api/V1/Service/PaymentBoontermService.php
+++ app/Api/V1/Service/PaymentBoontermService.php
@@ -17,7 +17,7 @@ class PaymentBoontermService{
           	"tel": "'.$order->user->phone.'", 
           	"cust_name": "'.$order->user->name.'", 
             "cust_lastname": "'. $order->user->surname.'",  
-          	"price": "'. ($order->total_price*100).'",  
+          	"price": '. $order->total_price.',  
           	"ref_id": "'.$order->order_no.'", 
           	"valid_day": '.$valid_day.', 
           	"valid_hour": '.$valid_hour.'
